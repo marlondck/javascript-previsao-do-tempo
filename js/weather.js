aeaab95b9e1c7cea4c859537941d42b37245984a
marlondck <15050029+marlondck@users.noreply.github.com>
2021-03-16 15:15:04 -0400

    implementando fetch

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,22 @@
 const APIKey = 'GrZtnorC3Pt16ZH3pUQJBDD51HLyMs6j'
+const getCityUrl = cityName =>
+  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+  try {
+    const cityUrl = getCityUrl(cityName)
+    const response = await fetch(cityUrl)
+
+    if(!response.ok) {
+      throw new Error('Não foi possível obter os dados.')
+    }
+
+    const [cityData] = await response.json()
+    return cityData
+
+  } catch ({ name, message }) {
+    console.log(`${name}: ${message}`)
+  }
+}
+
+getCityData('Campo Novo do Parecis')
